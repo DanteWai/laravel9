91e2c40fa03bb19f8a0a67696b670537c75dbb98
Dmitry Safronov <aa502@yandex.ru>
2022-06-11 14:50:33 +0700

    Базовый шаблон

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
 
+
+mix.sass('resources/css/app.scss', '/css').options({
+    autoprefixer:true
+})
+
+mix.js('resources/js/app.js', 'public/js')
 mix.js('resources/js/rater.js', 'public/js')
 mix.js('resources/js/dashboard.js', 'public/js')
