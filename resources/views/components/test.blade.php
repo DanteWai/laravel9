623b915e1556eb723a92039627b503312076af36
Dmitry Safronov <aa502@yandex.ru>
2022-06-17 23:58:39 +0700

    Тестовые блейд компоненты

--- resources/views/components/test.blade.php
+++ resources/views/components/test.blade.php
@@ -1,21 +1,13 @@
+@props([
+    'heading',
+    'footer',
+])
+
 <div {{$attributes->merge(['class' => 'test','disabled' => true])}}>
     <div {{$header->attributes->class(['test2'=> true])}}>
         {{$header}}
     </div>
-
-    <div>
-        hello, count is: {{$count}}
-    </div>
-    <div>
-        {{$menuItems->count()}}
-    </div>
-
-    <div>
-        {{$test('string')}}
-    </div>
-
     <div>
         {{$slot}}
     </div>
-
 </div>
---
+++ resources/views/components/testOrchid.blade.php
@@ -0,0 +1,3 @@
+<div>
+    hello orchid
+</div>
